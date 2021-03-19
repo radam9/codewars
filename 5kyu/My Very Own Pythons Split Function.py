4e97a27a7a54ada18404d93e05b413207b1d0cd9
Adam Saleh <radam9@gmail.com>
2021-03-19 15:53:13 +0100

    add my very own split function

--- 5kyu/My Very Own Pythons Split Function.py
+++ 5kyu/My Very Own Pythons Split Function.py
@@ -1,38 +1,67 @@
-# https://www.codewars.com/kata/55e0467217adf9c3690000f9/train/python
+# https://www.codewars.com/kata/55e0467217adf9c3690000f9
 # 5 Kyu
 # My Very Own Python's Split Function
 
+
 # My Solution
+import re
+
+
 def my_very_own_split(string, delimiter=None):
     if delimiter == "":
-        raise
-    delimiter = delimiter or " "
-    temp = ""
-    new_string = ""
-    for s in string:
-        if s == "\t" and temp == "\t":
-            continue
-        else:
-            new_string += ""
-        if s != temp:
-            new_string += s
-        temp = s
+        raise ValueError("no delimiter provided")
+    if len(string) == 0:
+        yield ""
+    if not delimiter:
+        delimiter = delimiter or " "
+        string = re.sub(r"\t+", " ", string)
+
     d = len(delimiter)
-    print(f"String: {string}\nDelimiter: '{delimiter}'")
-    while True:
+    while len(string):
         index = string.find(delimiter)
-        if index == 0:
-            string = string[d::]
+        if index == 0 and len(string) == d:
+            string = ""
             yield ""
-        if index == -1:
-            if string:
-                yield string
+        if index == -1 and string:
+            yield string
             break
-        part = string[:index]
+        yield string[:index]
         string = string[index + d :]
-        yield part
 
 
-for a in my_very_own_split("abc def ghi jklmno    prrrr", " "):
-    print(a)
-x = 0
+# Codewars Solution
+import re
+
+
+def my_very_own_split(string, delimiter=None):
+    if delimiter == "":
+        raise ValueError("empty delimiter")
+    if delimiter == None:
+        delimiter = "\s+"
+    else:
+        delimiter = re.escape(delimiter)
+    pos = 0
+    for m in re.finditer(delimiter, string):
+        yield string[pos : m.start()]
+        pos = m.end()
+    yield string[pos:]
+
+
+# Codewars no re
+def my_very_own_split(string, delimiter=None):
+
+    if delimiter == "":
+        raise ValueError
+
+    if delimiter is None:
+        string = string.replace("\t", " ")
+        while "  " in string:
+            string = string.replace("  ", " ")
+        delimiter = " "
+
+    last, l = 0, len(delimiter)
+    for i in range(len(string)):
+        if string[i : i + l] == delimiter:
+            yield string[last:i]
+            last = i + l
+    yield string[last:]
