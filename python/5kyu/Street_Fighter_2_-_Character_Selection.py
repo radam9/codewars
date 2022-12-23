02921a5a755d3bef9b04ec2ea94b16127c22e4b1
Adam Saleh <radam9@gmail.com>
2022-12-23 21:59:35 +0100

    fix python solution

--- python/5kyu/Street_Fighter_2_-_Character_Selection.py
+++ python/5kyu/Street_Fighter_2_-_Character_Selection.py
@@ -15,3 +15,19 @@ def street_fighter_selection(fighters, position, moves):
         x = (x + dx) % len(fighters[y])
         chars.append(fighters[y][x])
     return chars
+
+# My Second solution
+def super_street_fighter_selection(fighters, position, moves):
+    directions = {"right": [0, 1], "left": [0, -1], "up": [-1, 0], "down": [1, 0]}
+    chars = []
+    y, x = position
+    for move in moves:
+        dy, dx = directions[move]
+        y += dy
+        if not 0 <= y < len(fighters) or not fighters[y][x]:
+            y -= dy
+        x = (x + dx) % len(fighters[y])
+        while not fighters[y][x]:
+            x = (x + dx) % len(fighters[y])
+        chars.append(fighters[y][x])
+    return chars
